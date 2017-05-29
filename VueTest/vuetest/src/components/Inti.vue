c335e4259c364cfaf8fe35bfd6be4f6d64ba64ad
Miguel Angel Ardon Enriquez <miguel.ardon@unitec.edu>
2017-05-29 17:07:14 -0600

    Modificaciones de inti.vue

--- VueTest/vuetest/src/components/Inti.vue
+++ VueTest/vuetest/src/components/Inti.vue
@@ -3,7 +3,15 @@
 		<div class="prueba"><h1>{{message}}</h1></div>
 		<input type="text" placeholder="texto" v-model="message">
 		<button v-on:click="sendHelloWorld()">Hola</button>
-		<div class="prueba"><h1>{{message}}</h1></div>
+		<button v-on:click="toggleShow()">toggle</button>
+		<button v-on:click="addRecord()">Add Record</button>
+		<button v-on:click="removeRecord()">Remove Record</button>
+		<div v-if="showDiv==='false'">
+			<h1>cualquier cosa</h1>
+		</div>
+		<div v-for="record in list">
+			<h3>{{record}}</h3>
+		</div>
 	</div>
 </template>
 
@@ -15,7 +23,9 @@
 		data(){
 			return {
 				nombre:'Inti Velasquez',
-				message: 'Hola como estan?'
+				message: 'Hola como estan?',
+				showDiv: 'true',
+				list: [ ]
 			}
 		},
 		components: {
@@ -24,6 +34,19 @@
 		methods: {
 			sendHelloWorld(){
 				this.message='Mi nombre es inti'
+			},
+			toggleShow(){
+				if(this.showDiv==='true'){
+					this.showDiv='false';
+				}else{
+					this.showDiv='true';
+				}
+			},
+			addRecord(){
+				this.list.push(this.message);
+			},
+			removeRecord(){
+				this.list.pop();
 			}
 		},
 		beforeMount(){
@@ -34,6 +57,6 @@
 
 <style scoped>
 	.prueba{
-		background-color: gray;
+		background-color: brown;
 	}
 </style>
